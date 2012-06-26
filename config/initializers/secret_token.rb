635cc076b21881ff0b251e7b038aac7e522ab28f
Jarek Plonski <jaroslaw.plonski@gmail.com>
2012-06-27 00:55:21 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZeroXero01::Application.config.secret_token = 'bfb258d5f881389155a70acfb2190a1a63e367bc0587a9580f47ad2afb56282667e2bc49beb8626d30c4f551c26ed6908921a014fcde99caf729efd69f94b62f'
